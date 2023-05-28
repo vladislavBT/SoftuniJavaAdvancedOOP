8ae1ea6ec5844ae41fc68183dec9038f0c4a0745
vladislavBT <vladislavtulilov12345678@gmail.com>
2023-05-28 21:13:26 +0300

    InterfaceAndAbstractClasses2

---
+++ src/InterfaceAndAbstractClasses/CarShopExtend/Rentable.java
@@ -0,0 +1,6 @@
+package InterfaceAndAbstractClasses.CarShopExtend;
+
+public interface Rentable {
+    Integer getMinRentDay();
+    Double getPricePerDay();
+}
---
+++ src/InterfaceAndAbstractClasses/CarShopExtend/Sellable.java
@@ -0,0 +1,5 @@
+package InterfaceAndAbstractClasses.CarShopExtend;
+
+public interface Sellable {
+    Double getPrice();
+}
